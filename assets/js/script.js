79a885a5506d6ac5d19757fdbb96e69f62acd2f9
robappleby1 <robappleby1@sky.com>
2022-04-11 05:41:49 +0000

    Card images added to js

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", ( => {});
+
+//constants
+const section = document.querySelector('section');
+const playerLivesCount = document.querySelector('span')
+const playerLives = 6;
+
+playerLivesCount.textContent = playerLives;
+
+//cards created with images
+let iconsArray = [
+    {name: 'brown', img:'assets/images/jacob_brown.png'},
+    {name: 'vrancic', img:'assets/images/mario_vrancic.jpeg'},
+    {name: 'oneill', img:'assets/images/michael_oneill.jpeg'},
+    {name: 'powell', img:'assets/images/nick_powell.jpeg'},
+    {name: 'shawcross', img:'assets/images/jacob_ryan_shawcross.jpeg'},
+    {name: 'clucas', img:'assets/images/sam_clucas.jpeg'},
+    {name: 'ince', img:'assets/images/tom_ince.jpeg'},
+    {name: 'campbell', img:'assets/images/tyrese_campbell.jpeg'},
+    {name: 'shirts', img:'assets/images/stoke_shirts.jpeg'},
+
+    {name: 'brown', img:'assets/images/jacob_brown.png'},
+    {name: 'vrancic', img:'assets/images/mario_vrancic.jpeg'},
+    {name: 'oneill', img:'assets/images/michael_oneill.jpeg'},
+    {name: 'powell', img:'assets/images/nick_powell.jpeg'},
+    {name: 'shawcross', img:'assets/images/jacob_ryan_shawcross.jpeg'},
+    {name: 'clucas', img:'assets/images/sam_clucas.jpeg'},
+    {name: 'ince', img:'assets/images/tom_ince.jpeg'},
+    {name: 'campbell', img:'assets/images/tyrese_campbell.jpeg'},
+    {name: 'shirts', img:'assets/images/stoke_shirts.jpeg'},
+
+];
+
+
+
+
+});
