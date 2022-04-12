7aa7ad8d7e5ec287f000dfb2f4003e4175d46080
robappleby1 <robappleby1@sky.com>
2022-04-12 15:11:08 +0000

    Game board in place and drop shadow added behind badge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,37 @@
-//constants.
-const section = document.querySelector('section');
-const playerLivesCount = document.querySelector('span')
-const playerLives = 6;
+const clubCards = [
+    {name: "brown", img: "assets/images/brown.png"},
+    {name: "vrancic", img: "assets/images/mario_vrancic.jpeg"},
+    {name: "oneill", img: "assets/images/michael_oneill.jpeg"},
+    {name: "powell", img: "assets/images/nick_powell.jpeg"},
+    {name: "shawcross", img: "assets/images/ryan_shawcross.jpeg"},
+    {name: "clucas", img: "assets/images/sam_clucas.jpeg"},
+    {name: "ince", img: "assets/images/tom_ince.jpeg"},
+    {name: "campbell", img: "assets/images/tyrese_campbell.jpeg"},
+    {name: "brown", img: "assets/images/brown.png"},
+    {name: "vrancic", img: "assets/images/mario_vrancic.jpeg"},
+    {name: "oneill", img: "assets/images/michael_oneill.jpeg"},
+    {name: "powell", img: "assets/images/nick_powell.jpeg"},
+    {name: "shawcross", img: "assets/images/ryan_shawcross.jpeg"},
+    {name: "clucas", img: "assets/images/sam_clucas.jpeg"},
+    {name: "ince", img: "assets/images/tom_ince.jpeg"},
+    {name: "campbell", img: "assets/images/tyrese_campbell.jpeg"},
+    
+]
+    
+clubCards.sort(() => 0.5 - Math.random());
 
-playerLivesCount.textContent = playerLives;
 
-//cards created with images.
-const getData = () => [
+const gameDisplay = document.querySelector('.game-grid')
 
-    {name: 'brown', img:'assets/images/jacob_brown.png'},
-    {name: 'vrancic', img:'assets/images/mario_vrancic.jpeg'},
-    {name: 'oneill', img:'assets/images/michael_oneill.jpeg'},
-    {name: 'powell', img:'assets/images/nick_powell.jpeg'},
-    {name: 'shawcross', img:'assets/images/jacob_ryan_shawcross.jpeg'},
-    {name: 'clucas', img:'assets/images/sam_clucas.jpeg'},
-    {name: 'ince', img:'assets/images/tom_ince.jpeg'},
-    {name: 'campbell', img:'assets/images/tyrese_campbell.jpeg'},
-    {name: 'shirts', img:'assets/images/stoke_shirts.jpeg'},
 
-    {name: 'brown', img:'assets/images/jacob_brown.png'},
-    {name: 'vrancic', img:'assets/images/mario_vrancic.jpeg'},
-    {name: 'oneill', img:'assets/images/michael_oneill.jpeg'},
-    {name: 'powell', img:'assets/images/nick_powell.jpeg'},
-    {name: 'shawcross', img:'assets/images/jacob_ryan_shawcross.jpeg'},
-    {name: 'clucas', img:'assets/images/sam_clucas.jpeg'},
-    {name: 'ince', img:'assets/images/tom_ince.jpeg'},
-    {name: 'campbell', img:'assets/images/tyrese_campbell.jpeg'},
-    {name: 'shirts', img:'assets/images/stoke_shirts.jpeg'},
+function createBoard() {
+    for (let i = 0; i < 16; i++) {
+        const card = document.createElement("img")
+        card.setAttribute('src', 'assets/images/card-back.png')
+        card.setAttribute('data-id', i)
+        console.log(card, i)
+        gameDisplay.append(card)
+    }
 
-];
-
-const randomize = () => {
-    const cardData = getData();
-
-    cardData.sort(() => Math.random() - 0.5)
-    return cardData;
-   
 }
-
-randomize();
-
-//card generator
-const cardGenerator = () => {
-    cardData = randomize();
-
-
-cardData.forEach(item => {
-    const card = document.createElement("div");
-    const face = document.createElement("img");
-    const back = document.createElement("div");
-    card.classList = 'card';
-    face.classList = 'face';
-    back.classList = 'back';
-
-    //attach the cards
-
-section.appendChild(card);
-card.appendChild(face);
-card.appendChild(back);
-
-});
-
-};
-
-
-cardGenerator();
-
-
-  
+createBoard()
