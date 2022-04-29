b99adf2140f04d085bcee9b070c1bc1dd77ada00
robappleby1 <robappleby1@sky.com>
2022-04-29 10:38:10 +0000

    Fix media links in readme

--- assets/js/script.test.js
+++
@@ -1,7 +0,0 @@
-beforeAll (() => {
-    let fs = require ("fs");
-    let fileContents = fs.readFileSync ("index.html", "utf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
-})
